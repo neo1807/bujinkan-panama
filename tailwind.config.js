9e82d7b43944e0cbf10af6e85487cc587b8832de
Javier Cardenas <neo1807@gmail.com>
2022-07-21 16:56:03 -0500

    chore: remove unused plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,5 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
   ]
 }

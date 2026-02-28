c3d46205ee8215ae46b9ce49c1944a9a977281cd
Javier Cardenas <neo1807@gmail.com>
2026-02-28 16:53:11 -0500

    use layouts

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/site.webmanifest");
   return {
-    dir: { input: "src", output: "_site" },
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts",
+    },
   };
 };
